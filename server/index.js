6f51c82a11a4a7001cf103eb6930cf7bde10669f
Ashutosh <104207846+yadavashutosh708@users.noreply.github.com>
2023-03-02 14:27:29 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -12,40 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 
-/////Defining the schema and model
-const noteSchema =  new mongoose.Schema({
-  content: String,
-  important: Boolean
-});
-
-noteSchema.set('toJSON',{
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = new mongoose.model('Note', noteSchema);
-
-
-var notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
 
 app.get("/api/", (req, res) => res.send("<h1>Hello World!</h1>"));
